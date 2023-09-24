5ed1e60c8f34612ae93e0b0c02399dc22ea85e17
ihab-soufiane <ihab.soufiane@gmail.com>
2023-09-25 00:24:04 +0200

    feature of hotels

--- src/app/Hotels/hotel/hotel.component.ts
+++ src/app/Hotels/hotel/hotel.component.ts
@@ -1,11 +1,47 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-
+import { ActivatedRoute, Router } from '@angular/router';
+import { AdminService } from 'src/app/admin-service.service';
 @Component({
   selector: 'app-hotel',
   templateUrl: './hotel.component.html',
   styleUrls: ['./hotel.component.css']
 })
 export class HotelComponent {
+
+  ListHotels:any=[]
+
+  dataHotel={
+    Nom:'',
+    NbreEtoile:'',
+    Description:'',
+    image:'',
+    Prix:0,
+    id:'',
+  }
+
+  messageSuccess=''
+  constructor(private ds:AdminService,private route:Router ,private act:ActivatedRoute) {
+   
+    this.ds.gettAllHotel().subscribe(data=>{
+      console.log(data)
+      this.ListHotels=data
+    })
+    
+   }
+
+  
+  getdata(Nom:string,NbreEtoile:string,Description:string,Prix:number,image:string,id:any){
+    this.messageSuccess=''
+    this.dataHotel.Nom=Nom
+    this.dataHotel.NbreEtoile=NbreEtoile
+    this.dataHotel.Description=Description
+    this.dataHotel.Prix=Prix
+    this.dataHotel.image=image
+    this.dataHotel.id=id
+    console.log(this.dataHotel)
+    this.route.navigate(['/list'])
+
+  }
 
 }
